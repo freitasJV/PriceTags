20c20f75231f697a38acbffc84463b1e1000f279
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-02 17:39:06 -0300

    Add project files.

---
+++ PriceTags/Entities/ImportedProduct.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace PriceTags.Entities
+{
+    class ImportedProduct: Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+        public override string PriceTag()
+        {
+            return $"{Name} R${TotalPrice().ToString("F2", CultureInfo.InvariantCulture)} (Custos de importação: R${CustomsFee.ToString("F2", CultureInfo.InvariantCulture)})";
+        }
+    }
+}
---
+++ PriceTags/Program.cs
@@ -0,0 +1,72 @@
+﻿using PriceTags.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace PriceTags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Digite o número de produtos: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Dados do produto #{i}:");
+                Console.WriteLine("Tipo do produto:");
+                bool valido = false;
+                string tipo;
+                do
+                {
+                    Console.Write("Digite c para Comum, u para usado ou i para importado: ");
+                    tipo = Console.ReadLine();
+
+                    if (tipo.ToUpper().Equals("C") || tipo.ToUpper().Equals("U") || tipo.ToUpper().Equals("I"))
+                    {
+                        valido = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Escolha um tipo válido");
+                    }
+                } while (!valido);
+
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Preço: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (tipo.ToUpper().Equals("U"))
+                {
+                    Console.Write("Data de fabricação (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+                }
+
+                else if (tipo.ToUpper().Equals("I"))
+                {
+                    Console.Write("Custos de importação: ");
+                    double fee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, fee));
+                }
+
+                else
+                {
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Etiquetas:");
+
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p.PriceTag());
+            }
+        }
+    }
+}
